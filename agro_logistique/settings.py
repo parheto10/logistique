c0739d722ff9f08790a23ae409d4ebfd027058f3
unknown <parheto10@gmail.com>
2022-06-24 14:43:59 +0000

    Second Commit

--- agro_logistique/settings.py
+++ agro_logistique/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -181,5 +181,5 @@ MEDIA_ROOT = (os.path.join(BASE_DIR, 'static', 'medias'))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Activate django Heroku
-django_heroku.settings(locals())
+# # Activate django Heroku
+# django_heroku.settings(locals())
